eb583d32c701698ec97df944e41078a7665dc8c2
Guzmán Brasó <guzman@guruhub.com.uy>
2012-11-06 16:19:31 -0200

    vapaahi

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,27 @@
 #
 # Class to install arno-iptables-firewall script and configure it.-
 #
-class arno {
+class arno(
+	$ext_if,
+	$int_net,
+	$services_tcp = '22',
+	$int_if
+) {
 	
+	# Setup later as args and move to defaults.pp
+	$preseed_file = '/root/.puppet-arno.preseed'
+
+	# Preseed file for installing
+	file { $preseed_file :
+		ensure => present,
+		owner  => root,
+		group  => root,
+		mode   => 600
+	}
+
 	package{ 'arno-iptables-firewall' :
 		ensure => present,
+		responsefile => $preseed_file,
 	}
+
 }
