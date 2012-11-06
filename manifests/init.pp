003a3a7771d1d0adf006bdeff55f2f9d7f41bd42
Guzmán Brasó <guzman@guruhub.com.uy>
2012-11-06 16:53:19 -0200

    vapaahi

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,11 +5,13 @@ class arno(
 	$ext_if,
 	$int_net,
 	$services_tcp = '22',
-	$int_if
+	$int_if,
+	$patch_public_nat_from_inside = false
 ) {
 	
 	# Setup later as args and move to defaults.pp
 	$preseed_file = '/root/.puppet-arno.preseed'
+	$patch_file   = '/root/.puppet-arno.patch'
 
 	# Preseed file for installing
 	file { $preseed_file :
@@ -23,6 +25,16 @@ class arno(
 	package{ 'arno-iptables-firewall' :
 		ensure => present,
 		responsefile => $preseed_file,
+		require      => File[ $preseed_file ]
 	}
 
+	if $patch_public_nat_from_inside {
+		file { $patch_file :
+			owner  => root,
+			group  => root,
+			mode   => 600,
+			source => 'puppet:///arno/arno.patch'
+		}
+
+	}
 }
